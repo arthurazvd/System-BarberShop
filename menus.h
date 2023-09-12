279ceec413e7d15c4f60e7d773da0d8fca0e5049
Arthur <azvd.arthur@gmail.com>
2023-09-12 14:12:01 -0300

    Modularização das telas de menu

---
+++ menus.h
@@ -0,0 +1,54 @@
+#ifndef MENU_H
+#define MENU_H
+
+// TELAS
+
+int tela_inicial();
+
+int menu_inicial();
+
+int menu_agenda();
+
+int menu_clientes();
+
+int menu_precos();
+
+int menu_maquinas();
+
+int sobre();
+
+// SUBMENUS
+
+int agd_horario();
+
+int agd_pesquisa();
+
+int agd_agendar();
+
+int agd_cancelamento();
+
+int cli_lista();
+
+int cli_pesquisa();
+
+int cli_cadas();
+
+int cli_del();
+
+int pre_lista();
+
+int pre_pesquisa();
+
+int pre_cadas();
+
+int pre_del();
+
+int maq_lista();
+
+int maq_pesquisa();
+
+int maq_cadas();
+
+int maq_del();
+
+#endif
